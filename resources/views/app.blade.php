c378dd946d3e143a9ce1b785aa4eade3d6b63922
Hector Caparros <dompi@Mac-Studio-de-Hector.local>
2022-08-27 10:46:02 -0500

    first commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+@vite('resources/css/app.css')
+<title>Devstagraml</title>
+
+
+</head>
+<body>
+     <header class="p-5 border-b bg-white shadow">
+        <h1 class="text-3xl font-black">
+            DevStagram
+        </h1>
+
+
+     </header>
+</body>
---
+++ resources/views/principal.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite('resources/css/app.css')
+        <title>Pagina principal</title>
+
+    
+    </head>
+    <body>
+        <h1 class="Text-2xl">Página principal</h1>
+    </body>
+</html>
